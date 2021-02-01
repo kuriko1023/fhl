4921c3037c6ddbf3804b22020590fb0aeaf0dae3
Shiqing <shiqing-thu18@yandex.com>
2021-02-01 15:00:17 +0800

    整理 backend 代码格式，建立 Go module

--- backend/logic.go
+++ backend/logic.go
@@ -1,38 +1,30 @@
+package main
 
-/***所有高频词组成的列表，包含单字和双字
- **键表示高频词，值表示该词出现的频数***/
- var hotWords map[string]int
+// 所有高频词组成的列表，包含单字和双字
+// 键表示高频词，值表示该词出现的频数
+var hotWords map[string]int
 
-
-/***检查一句诗词是否符合规则***/
-func check(sentence string, keywords []string) bool{
-
+// 检查一句诗词是否符合规则
+func check(sentence string, keywords []string) bool {
+	return false
 }
 
-
-/***返回一句诗词中的所有高频词，按出现次数降序排序；若无，返回空列表***/
-func getHotWords(sentence string) []string{
-
+// 返回一句诗词中的所有高频词，按出现次数降序排序；若无，返回空列表
+func getHotWords(sentence string) []string {
+	return nil
 }
 
-
-/***生成普通飞花题目备选列表，返回 count 个不重复的单字/词***/
-func generateA(count int) []string{
-
+// 生成普通飞花题目备选列表，返回 count 个不重复的单字/词
+func generateA(count int) []string {
+	return nil
 }
 
-
-/***生成超级飞花题目，返回长度为 n 和 m 的字符串列表***/
-func generateB() ([]string, []string){
-
+// 生成超级飞花题目，返回长度为 n 和 m 的字符串列表
+func generateB() ([]string, []string) {
+	return nil, nil
 }
 
-
-/***生成2个长度为n的关键词组作为谜之飞花题目***/
-func generateC() ([]string, []string){
-	
+// 生成 2 个长度为 n 的关键词组作为谜之飞花题目
+func generateC() ([]string, []string) {
+	return nil, nil
 }
-
-
-
-
