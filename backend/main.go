c8ebbb2b9e6bb4fff800a48ed1716291600605e7
kurikoneko <yangxr18@mails.tsinghua.edu.cn>
2021-02-02 21:36:05 +0800

    修改了谜之飞花令的算法，生成的题目更具普适性

--- backend/main.go
+++ backend/main.go
@@ -7,14 +7,15 @@ import (
 func main() {
 	fmt.Println("Hello, world!")
 	initDataset()
-	for i := 0; i < 10; i++ {
-		fmt.Println(generateA(5, 3))
-	}
-	for i := 0; i < 10; i++ {
-		fmt.Println(generateC(3, 10))
-	}
-	/*sl1, sl2 := generateD(8)
+	// for i := 0; i < 10; i++ {
+	// 	fmt.Println(generateA(5, 3))
+	// }
+	// for i := 0; i < 10; i++ {
+	// 	fmt.Println(generateC(3, 10))
+	// }
+	furtherInit()
+	sl1, sl2 := generateD(8)
 	for i := 0; i < 8; i++ {
 		fmt.Println(sl1[i], sl2[i])
-	}*/
+	}
 }
