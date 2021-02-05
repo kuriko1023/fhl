a074772e23d6e959b98d6699a454b2dfde9dee1e
kuriko <yangxr18@mails.tsinghua.edu.cn>
2021-02-05 13:29:22 +0800

    add simple logic in frontend

---
+++ frontend/src/components/Subject.vue
@@ -0,0 +1,32 @@
+<template>
+  <view>
+    <view>
+      <text v-for="item in data.subject1" :key="item">
+        {{item}}
+      </text>
+    </view>
+    <view>
+      <text>
+        {{data.subject2}}
+      </text>
+    </view>
+  </view>
+</template>
+
+<script>
+export default {
+  name: "Subject",
+  props: {
+      data: {
+        type: Object,
+      },
+      mode: {
+        type: String
+      }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/pages/EndPage/EndPage.vue
@@ -0,0 +1,51 @@
+<template>
+  <view>
+    <view>
+      <subject-block :data="subject" mode="C"/>
+    </view>
+    <view>
+      <history-block :data="history"/>
+    </view>
+    <button @click="onBack">返回</button>
+  </view>
+</template>
+
+<script>
+import Subject from "@/components/Subject";
+import History from "@/components/History";
+export default {
+name: "EndPage",
+  components:{
+    "subject-block": Subject,
+    "history-block": History
+  },
+  data() {
+    return{
+      subject: {
+        "subject1": ["雁", "古", "梦"],
+        "subject2": "长 舟 送 寄 事 神 不 生 西风 多少",
+      },
+      history: [
+        {
+          "value": "长风万里送秋雁"
+        },
+        {
+          "value": "千古兴亡多少事"
+        }
+      ]
+    }
+  },
+  methods:{
+    onBack(){
+      uni.redirectTo({
+        'url': '/pages/RoomPage/RoomPage'
+      })
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
