c26f685c57d19242feb6c8453e0b77e3a79753b9
Shiqing <shiqing-thu18@yandex.com>
2021-05-29 01:02:50 +0800

    实现客户端获取用户信息

--- frontend/src/pages/StartPage/StartPage.vue
+++ frontend/src/pages/StartPage/StartPage.vue
@@ -13,6 +13,20 @@ name: "StartPage",
   methods: {
     onEnter(){
       console.log("1")
+
+      // 发布时使用 getUserProfile
+      // uni.getUserProfile({
+      // 测试时使用 getUserInfo 会返回「微信用户」和灰色头像
+      uni.getUserInfo({
+        desc: '用于向其他玩家展示头像和昵称',
+        success: (res) => {
+          console.log(res.userInfo.nickName, res.userInfo.avatarUrl)
+        },
+        fail: () => {
+          console.log('getUserProfile() failed')
+        },
+      })
+
       uni.navigateTo({
         url: "/pages/RoomPage/RoomPage"
       })
