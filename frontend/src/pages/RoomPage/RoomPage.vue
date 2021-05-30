6657f90c5f03eb20fe16f074b617f63c794b7c74
Ayu <backyard@ayu.land>
2021-05-31 00:14:05 +0800

    实现客户端接收 WebSocket 的消息队列

--- frontend/src/pages/RoomPage/RoomPage.vue
+++ frontend/src/pages/RoomPage/RoomPage.vue
@@ -1,26 +1,59 @@
 <template>
   <view>
-    <button @click="sitDown"> 座位1 </button>
-    <button @click="sitDown"> 座位2 </button>
-    <button @click="onStart"> 开始游戏 </button>
+    <template v-if='!connected'>
+      {{ status }}
+    </template>
+    <template v-else>
+      <p>房主：{{ host }}</p>
+      <p>房主状态 {{ hostStatus }}</p>
+      <button @click="sitDown">坐下</button>
+      <button @click="startGame">开始游戏</button>
+    </template>
   </view>
 </template>
 
 <script>
 export default {
-name: "RoomPage",
-  onReady: function() {
+  name: "RoomPage",
+  data() {
+    return {
+      connected: false,
+      status: '- 状态 -',
 
+      host: '',
+      hostStatus: '',
+    };
+  },
+  onLoad() {
+    uni.connectSocket({
+      url: 'wss://flyhana.starrah.cn/channel/my/!kuriko1023',
+      success: () => {
+        // setTimeout(() => this.connected = true, 1000)
+        this.connected = true;
+        this.registerSocketMessageListener();
+      },
+      fail: () => {
+        this.status = '连接失败';
+      },
+    });
   },
   methods: {
-    sitDown(){
-
+    onSocketMessage() {
+      console.log('on message!');
+      console.log('peeked message ', this.peekSocketMessage());
     },
-    onStart(){
-        uni.redirectTo({
-          url: "/pages/ChoosePage/ChoosePage"
-        })
-      }
+    sitDown() {
+      uni.sendSocketMessage({
+        data: {
+          type: 'ready',
+        },
+      });
+    },
+    startGame() {
+      uni.redirectTo({
+        url: "/pages/ChoosePage/ChoosePage"
+      })
+    }
   }
 }
 </script>
