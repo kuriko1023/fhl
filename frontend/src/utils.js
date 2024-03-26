8a6f45d83731eb2d83407ff193114a34d7d1cfd4
Ayu <claire@ayu.land>
2024-03-26 16:36:28 +0800

    Prepare for webpage port

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,5 +1,7 @@
-const apiServer = 'https://fhxl.ayuu.ink';
-const wsServer = 'wss://fhxl.ayuu.ink';
+// const apiServer = 'https://fhxl.ayuu.ink';
+// const wsServer = 'wss://fhxl.ayuu.ink';
+const apiServer = 'http://localhost:2310';
+const wsServer = 'ws://localhost:2310';
 
 const staticRes = (file) => `${apiServer}/static/${file}`;
 
